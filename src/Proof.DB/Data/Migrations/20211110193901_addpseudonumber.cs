4823e165ce8e66a870d525abbb0ab78e77056434
g00fy88 <maxsegelken@gmail.com>
2021-11-11 19:12:46 +0100

    feat: add data migration

---
+++ src/Proof.DB/Data/Migrations/20211110193901_addpseudonumber.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Poof.DB.Data.Migrations
+{
+    public partial class addpseudonumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PseudoNumber",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PseudoNumber",
+                table: "AspNetUsers");
+        }
+    }
+}
