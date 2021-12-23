6e89e3faf0a1188e4aca70472acaca64423beb6c
g00fy88 <maxsegelken@gmail.com>
2021-12-23 20:46:59 +0100

    feat: add db migration

---
+++ src/Proof.DB/Data/Migrations/20211223152833_add-memberships-and-initiators.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Poof.DB.Data.Migrations
+{
+    public partial class addmembershipsandinitiators : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InitiatorId",
+                table: "Transactions",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Picture",
+                table: "Fellowships",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Fellowships",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_InitiatorId",
+                table: "Transactions",
+                column: "InitiatorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_AspNetUsers_InitiatorId",
+                table: "Transactions",
+                column: "InitiatorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_AspNetUsers_InitiatorId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_InitiatorId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "InitiatorId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "Fellowships");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Fellowships");
+        }
+    }
+}
